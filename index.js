23f748ad8129c3da11c284d593d69da1360e424e
oriri04 <152679239+oriri04@users.noreply.github.com>
2023-12-27 21:58:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards() {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    debugger;
+  }
+  return names;
+}
+
+writeCards(names);
+
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+countDown(11); 
+
+
